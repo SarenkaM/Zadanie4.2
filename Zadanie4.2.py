bb2efb9e89c3da99ca6b7f57d6336c0ea109affa
Marcin Sarna <sarna077@gmail.com>
2023-01-28 15:53:18 +0100

    Add function documentation

--- Zadanie4.2.py
+++ Zadanie4.2.py
@@ -1,6 +1,9 @@
-def palindrom(x):
+def palindrom(word):
+    """
+    Check is given word palindrom
+    Argument: checking word"""
     first_list = []
-    for i in x:
+    for i in word:
         first_list.append(i)
     print(first_list == list(reversed(first_list)))
 
