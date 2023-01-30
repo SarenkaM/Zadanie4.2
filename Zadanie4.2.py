5eecc071eb13a74b182cbe700e5f3421d4bd7203
Marcin Sarna <sarna077@gmail.com>
2023-01-30 19:16:51 +0100

    Add second way

--- Zadanie4.2.py
+++ Zadanie4.2.py
@@ -7,9 +7,20 @@ def palindrom(word):
         first_list.append(i)
     print(first_list == list(reversed(first_list)))
 
+def palindrom_easy(word):
+    """
+    Check is given word palindrom
+    Argument: checking word"""
+    first_list = []
+    print(word[::1] == word[::-1])
+
 
 palindrom("kajak")
 palindrom("słowo")
 palindrom("123321")
 palindrom("potop")
 
+palindrom_easy("kajak")
+palindrom_easy("słowo")
+palindrom_easy("123321")
+palindrom_easy("potop")
