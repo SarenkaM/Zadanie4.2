4c2db698637afbb41b49e3f4f1ebea6fcf1fff7b
Marcin Sarna <sarna077@gmail.com>
2023-02-07 22:03:28 +0100

    Change functions

--- Zadanie4.2.py
+++ Zadanie4.2.py
@@ -1,26 +1,20 @@
-def palindrom(word):
-    """
-    Check is given word palindrom
-    Argument: checking word"""
-    first_list = []
-    for i in word:
-        first_list.append(i)
-    print(first_list == list(reversed(first_list)))
+
 
 def palindrom_easy(word):
     """
     Check is given word palindrom
     Argument: checking word"""
-    first_list = []
-    print(word[::1] == word[::-1])
+    palindrom = True
+    if word == word[::-1]:
+        print(palindrom)
+    else:
+        print(bool())
 
 
-palindrom("kajak")
-palindrom("słowo")
-palindrom("123321")
-palindrom("potop")
+
 
 palindrom_easy("kajak")
 palindrom_easy("słowo")
 palindrom_easy("123321")
 palindrom_easy("potop")
+palindrom_easy("pottop")
